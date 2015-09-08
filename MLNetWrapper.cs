c03cb5e6ed9282df359c161441158ffc8df91a1e
Francesco <francescot25@hotmail.it>
2015-09-08 12:09:38 +0200

    Added properties.

--- MLNetWrapper.cs
+++ MLNetWrapper.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MLApp;
+using System.IO;
+using System.Collections.ObjectModel;
 
 namespace MLNetWrapper
 {
+    /// <summary>
+    /// Specifies the training function to use. For more information see MATLAB's documentation.
+    /// </summary>
+    enum TrainFunctions { 
+        /// <summary>Levenberg-Marquardt backpropagation</summary>
+        LM,
+        /// <summary>Bayesian Regulation backpropagation.</summary>
+        BR,
+        /// <summary>Scaled conjugate gradient backpropagation.</summary>
+        SCG
+    };
+
+    /// <summary>
+    /// Lists the input and output pre/post-Processing functions.
+    /// </summary>
+    enum ProcessingFunctions
+    {
+        /// <summary>Normalize inputs/targets to fall in the range [−1, 1]</summary>
+        MAPMINMAX,
+        /// <summary>Normalize inputs/targets to have zero mean and unity variance</summary>
+        MAPSTD,
+        /// <summary>Extract principal components from the input vector</summary>
+        PROCESSPCA,
+        /// <summary>Process unknown inputs</summary>
+        FIXUNKNOWNS,
+        /// <summary>Remove inputs/targets that are constant</summary>
+        REMOVECONSTANTROWS
+    }
+
     /// <summary>
     /// Creates a MATLAB instance.
     /// </summary>
@@ -27,6 +55,120 @@ namespace MLNetWrapper
         /// <value>This property defines the number of inputs a network receives. It is a positive integer.</value>
         public int inputLayerSize { get; }
         public int hidden { get; }
+
+        private TrainFunctions trainFcn;
+        /// <summary>
+        /// Gets or sets the training algorithm.
+        /// </summary>
+        public TrainFunctions TrainFunction
+        {
+            get { return trainFcn; }
+            set
+            {
+                trainFcn = value;
+                wrapper.matlab.Execute(name + ".trainFcn = " + TrainFunctionToString(TrainFunction));
+            }
+        }
+
+        /// <summary>
+        /// Gets pre/post-processing functions to use on inputs and outputs. You can add/remove items.
+        /// </summary>
+        public ObservableCollection<ProcessingFunctions> ProcessFunctions { get; } = new ObservableCollection<ProcessingFunctions>();
+
+        private double trainRatio;
+        /// <summary>
+        /// Gets or sets the relative number of training samples to be selected from all samples,
+        /// compared to the relative numbers of validation samples valRatio, and test samples testRatio.
+        /// </summary>
+        /// <value>It is a positive double in the interval (0, 1).</value>
+        public double TrainRatio
+        {
+            get
+            {
+                return trainRatio;
+            }
+            set
+            {
+                if (value > 0 && value < 1)
+                {
+                    trainRatio = value;
+                    wrapper.matlab.Execute(name + ".divideParam.trainRatio = " + trainRatio.ToString());
+                }
+            }
+        }
+
+        private double valRatio;
+        /// <summary>
+        /// Gets or sets the relative number of validation samples to be selected from all samples,
+        /// compared to the relative numbers of training samples valRatio, and test samples testRatio.
+        /// </summary>
+        /// <value>It is a positive double in the interval (0, 1).</value>
+        public double ValRatio
+        {
+            get
+            {
+                return valRatio;
+            }
+            set
+            {
+                if (value > 0 && value < 1)
+                {
+                    valRatio = value;
+                    wrapper.matlab.Execute(name + ".divideParam.valRatio = " + valRatio.ToString());
+                }
+            }
+        }
+
+        private double testRatio;
+        /// <summary>
+        /// Gets or sets the relative number of test samples to be selected from all samples,
+        /// compared to the relative numbers of validation samples valRatio, and training samples testRatio.
+        /// </summary>
+        /// <value>It is a positive double in the interval (0, 1).</value>
+        public double TestRatio
+        {
+            get
+            {
+                return testRatio;
+            }
+            set
+            {
+                if (value > 0 && value < 1)
+                {
+                    testRatio = value;
+                    wrapper.matlab.Execute(name + ".divideParam.testRatio = " + testRatio.ToString());
+                }
+            }
+        }
+
+        private bool showWindow;
+        /// <summary>
+        /// Whether to show the training MATLAB's window or not;
+        /// </summary>
+        public bool ShowWindow
+        {
+            get { return showWindow; }
+            set
+            {
+                showWindow = value;
+                wrapper.matlab.Execute(name + ".trainParam.showWindow = " + Convert.ToInt32(showWindow).ToString());
+            }
+        }
+
+        private int epochs;
+        /// <summary>
+        /// Gets or sets the maximum number of training iterations before training is stopped.
+        /// </summary>
+        /// <value>It is a positive scalar.</value>
+        public int Epochs
+        {
+            get { return epochs; }
+            set
+            {
+                epochs = value;
+                wrapper.matlab.Execute(name + ".trainParam.epochs = " + Epochs.ToString());
+            }
+        }
 
         /// <summary>
         /// Name of this net in the current MATLAB workspace.
@@ -52,8 +194,37 @@ namespace MLNetWrapper
             //Create network in MATLAB
             name = "net" + wrapper.index.ToString(); wrapper.index++; //Set the workspace name and Increase the index as we're creating a new network
             wrapper.matlab.Execute(name + " = 0"); //Initialize the variable in the workspace
-            wrapper.matlab.Execute(@"cd c:\users\franc\desktop\"); //Change directory to our folder where we have some functions
-            wrapper.matlab.Execute(name + "= CreateNetwork(" + name + "," + hidden.ToString() + ")");
+            wrapper.matlab.Execute(@"trainFcn = 'trainlm'"); //Set the default training function, just because fitnet requires it, in any case we'll set it later
+            wrapper.matlab.Execute(name + " = fitnet(" + this.hidden + ", trainFcn)"); //Creates the function fitting neural network
+
+            //Set default values
+            this.TrainRatio = 0.75;
+            this.ValRatio = 0.15;
+            this.TestRatio = 0.15;
+            this.ShowWindow = true;
+            this.Epochs = 1000;
+            this.TrainFunction = TrainFunctions.LM;
+            this.ProcessFunctions.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(
+                delegate (object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+                {
+                    //When a new item is added/removed, set the new processing functions for both input and output
+                    if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add || e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+                    {
+                        string toexecute = "{";
+                        foreach (ProcessingFunctions f in this.ProcessFunctions)
+                            toexecute += "'" + ProcessingFunctionToString(f) + "',";
+                        toexecute = toexecute.Remove(toexecute.Length - 1) + "}";
+                        wrapper.matlab.Execute(name + ".input.processFcns = " + toexecute);
+                        wrapper.matlab.Execute(name + ".output.processFcns = " + toexecute);
+                    }
+                }
+            );
+            this.ProcessFunctions.Add(ProcessingFunctions.REMOVECONSTANTROWS);
+            this.ProcessFunctions.Add(ProcessingFunctions.MAPMINMAX);
+
+            //Train a single zero-example in order to be able to use Execute before training
+            wrapper.matlab.Execute("x = zeros(1, " + this.inputLayerSize.ToString() + ")");
+            wrapper.matlab.Execute(name + " = train(" + name + ", x', 0)"); //Configure the network for the number of inputs and a single output
         }
 
 
@@ -91,6 +262,41 @@ namespace MLNetWrapper
             wrapper.matlab.Execute("x = x'"); //Transpose, because MATLAB wants examples as columns
             value = wrapper.matlab.Execute(name + "(x)").Replace("ans", "").Replace("=", "").Replace("\n", ""); //Get and parse the answer
             return double.Parse(value);
+        }
+
+        //Some useful methods
+        private string TrainFunctionToString(TrainFunctions f)
+        {
+            switch(f)
+            {
+                case TrainFunctions.BR:
+                    return "trainbr";
+                case TrainFunctions.LM:
+                    return "trainlm";
+                case TrainFunctions.SCG:
+                    return "trainscg";
+                default:
+                    throw new Exception("Train Function not defined");
+            }
+        }
+
+        private string ProcessingFunctionToString(ProcessingFunctions f)
+        {
+            switch(f)
+            {
+                case ProcessingFunctions.FIXUNKNOWNS:
+                    return "fixunknowns";
+                case ProcessingFunctions.MAPMINMAX:
+                    return "mapminmax";
+                case ProcessingFunctions.MAPSTD:
+                    return "mapstd";
+                case ProcessingFunctions.PROCESSPCA:
+                    return "processpca";
+                case ProcessingFunctions.REMOVECONSTANTROWS:
+                    return "removeconstantrows";
+                default:
+                    throw new Exception("Processing Function not defined");
+            }
         }
 
 
--- Program.cs
+++ Program.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MLApp;
-using System.IO;
 using MLNetWrapper;
 
-namespace ProvaMatlab
+namespace Test
 {
     class Program
     {
@@ -17,9 +11,14 @@ namespace ProvaMatlab
             double[,] b = new double[3,1] { { 0 }, { 0.8 }, { 1 } };
             Wrapper wrapper = new Wrapper();
             Net net = new Net(wrapper, 2, 5);
-            net.Train(a, b);
             Console.WriteLine(net.Execute(new double[] { 0.9, 1 }));
-
+            //net.Train(a, b);
+            //Console.WriteLine(net.Execute(new double[] { 0.9, 1 }));
+            Console.WriteLine(net.TrainFunction);
+            net.TrainFunction = TrainFunctions.BR;
+            net.ProcessFunctions.Add(ProcessingFunctions.FIXUNKNOWNS);
+            Console.WriteLine(net.TrainFunction); Console.Read();
+            return;
             Net net2 = new Net(wrapper, 1, 5);
             net2.Train(new double[3,1] { { 0.1 }, { 0.2 }, { 0.3 } }, new double[3,1] { { 0.4 }, { 0.5 }, { 0.6 } });
             Console.WriteLine(net2.Execute(new double[] { 0.1 }));
